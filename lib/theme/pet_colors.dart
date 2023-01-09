10c962fc828565cb83c9e2882fd93edd7ace331e
Felipe Carvalho <feliped3carvalho@gmail.com>
2023-01-08 21:13:37 -0300

    Refatorado primeira tela removido componentes.

--- lib/theme/pet_colors.dart
+++ lib/theme/pet_colors.dart
@@ -4,6 +4,6 @@ class PetColors {
   static const petTextBlack = Color(0xff292826);
   static const petSymbols = Color(0xffFF5F50);
   static const petTextGray = Color(0xffC7C7C7);
-  static const petBacgroundHome = Color(0xffF2F2F2);
+  static const petBacgroundHome = Color.fromARGB(255, 214, 214, 214);
   static const petPrimary = Color(0xffFFFFFF);
 }
